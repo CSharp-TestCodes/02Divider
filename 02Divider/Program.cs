05cc556a03efc1c0ff772de0485e7496105927e2
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2023-05-10 23:18:07 +0530

    Updated

--- 02Divider/Program.cs
+++ 02Divider/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+for(int i =1; i<=100; i++)
+{
+    if (i % 15 == 0)
+    {
+        Console.WriteLine(i + " is divided by both 5 and 3");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine(i +" is divided by only 5");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine(i + " is divided by only 3");
+    }
+    else
+    {
+        Console.WriteLine(i + " is not devicible");
+    }
+}
